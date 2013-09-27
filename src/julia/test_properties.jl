9b829a39cd8ed8dc9e64665120097d3f7c542483
Cameron Smith <camrn86@gmail.com>
2013-09-27 02:26:23 -0400

    consider basic questions about individuals: what proportion of convergent individuals have different stable from initial state?

---
+++ src/julia/test_properties.jl
@@ -0,0 +1,36 @@
+using Graphs
+
+require("types.jl")
+require("individuals.jl")
+
+function testdifffininit(G,C)
+    # julia> testdifffininit(10,0.75)
+    # Nsame, Ndiff, Ndiff/(Ndiff+Nsame)
+    # (9634,366,0.0366)
+    Ndiff=0
+    Nsame=0
+    for i=1:10000
+        founder = zeros(Float64,G,G)
+
+        conflag=false
+        diffflag=false
+        finstate=[]
+        initstate=[]
+        while conflag!=true
+            for i=1:G^2
+                if rand()<C
+                    founder[i] = randn()
+                end
+            end
+            conflag, finstate, initstate=testconvergence(founder)
+        end
+
+        if sum([isapprox(finstate[i],initstate[i]) for i=1:length(finstate)])==G
+            Ndiff=Ndiff+1
+            diffflag=true
+        else
+            Nsame=Nsame+1
+        end
+    end
+    return Nsame, Ndiff, Ndiff/(Ndiff+Nsame)
+end
